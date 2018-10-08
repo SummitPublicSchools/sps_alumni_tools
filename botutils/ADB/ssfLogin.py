15d5a909af605b5a3ed3122abe12057c7ea080e8
bobbycupp <bcupp@summitps.org>
2018-10-08 13:26:00 -0700

    First publish of updated tools
    Publish Salesforce tools to Github.

---
+++ botutils/ADB/ssfLogin.py
@@ -0,0 +1,9 @@
+#!python3
+'''
+This file is just for saving login details (in clear text)
+To use, update the three member variables below to match
+your login credentials
+'''
+user = 'dataoperations+1@summitps.org'
+password = '5f9$@7#HR13A'
+token = 'JtAA48gIQYAx7sXqNOoQ3kN3'
