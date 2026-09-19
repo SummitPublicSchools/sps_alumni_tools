e8ef18e578353b2bd06c532b508b6f1ba9496cc8
howard_shen <hshen@summitps.org>
2026-09-18 21:29:02 -0400

    NPS student code

--- Warehouse_SQL/graduates.sql
+++ Warehouse_SQL/graduates.sql
@@ -1,12 +1,13 @@
 /*
  * Author: Howard Shen
- * Last Edit: July 20, 2022
+ * Last Edit: September 18, 2026
  * Purpose: This query grabs the list of graduates to upload to the NSC FTP server.
  * See this gitbook for complete instructions: https://app.gitbook.com/o/-LxjWyeokAsuZLyAr6q3/s/-MNUJzWQIebv9CNouxFt/
+ * 9/18/26 Edit: Add the code for dealing with NPS students
  */
 
 
-SET max_year = 2022;
+SET max_year = 2026;
 
 SELECT
     'PD3'
@@ -51,6 +52,11 @@ LEFT JOIN public.students_historical AS students
     ON graduates.student_id = students.student_id
     AND graduates.graduation_date = students.school_date
 LEFT JOIN public.sites_historical AS sites
-    ON graduates.site_id = sites.site_id
+    ON (
+        (graduates.site_id = sites.site_id AND sites.include_site) -- Do not map NPS sites onto themselves
+        OR (graduates.site_id = 9999995 AND sites.site_id = 5) --Denali NPS match to Denali
+        OR (graduates.site_id = 9999996 AND sites.site_id = 8) -- Tam NPS match to Tam
+        OR (graduates.site_id = 9999997 AND sites.site_id = 7) -- K2 NPS match to K2
+    )
     AND graduates.graduation_year = sites.academic_year
 WHERE graduation_year >= $max_year - 7
